b43bff60bfa8c3836a767b2516b40d727a09138c
talbot648 <charlietalbot648gmail.com>
2025-06-17 15:18:23 +0100

    added test for getting top movies

--- backend/main_test.go
+++ backend/main_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"Movie/db"
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
@@ -51,4 +54,42 @@ func TestRootHandlerWitherServer(t *testing.T) {
 		t.Errorf("handler returned unexpected body: got %v want %v", string(bodyBytes), expected)
 	}
 
+}
+
+func TestGetTopMoviesHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/api/topMovies", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(getTopMovies)
+
+	expected := []db.TopMovies{
+		{FilmName: "The Shawshank Redemption", AverageRating: 9.3, TotalVotes: 2345678},
+		{FilmName: "The Godfather", AverageRating: 9.2, TotalVotes: 2345678},
+		{FilmName: "The Dark Knight", AverageRating: 9.0, TotalVotes: 2345678},
+	}
+	expectedJSON, err := json.Marshal(expected)
+	if err != nil {
+		t.Fatalf("Failed to marshal expected JSON: %v", err)
+	}
+
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	if rr.Body.String() != string(expectedJSON) {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+
+	var actual []db.TopMovies
+	if err := json.Unmarshal(rr.Body.Bytes(), &actual); err != nil {
+		t.Fatalf("Failed to unmarshal response body: %v", err)
+	}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("handler returned unexpected body: got %v want %v", actual, expected)
+	}
 }
