66a382b3de56cb608b5c9936095f84f3f6428bdf
talbot648 <charlietalbot648gmail.com>
2025-06-18 14:26:46 +0100

    fixed film card config

--- frontend/src/components/FilmCard.jsx
+++ frontend/src/components/FilmCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import '../css/FilmCard.css'
+import '../css/filmCard.css'
 
 const FilmCard = ({ filmName, AverageRating, totalVotes, onClick }) => (
   <Card className="film-card m-2 shadow-sm" onClick={onClick}>
