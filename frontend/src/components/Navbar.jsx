8d75229a6a97ee406e1dd04806a35b6dd6f00e77
talbot648 <charlietalbot648gmail.com>
2025-06-24 17:14:14 +0100

    added expandable burger menu for navbar for different sizes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import { Outlet, NavLink } from "react-router-dom";
 import '../css/navbar.css';
+import { useState } from "react";
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <nav className="navbar navbar-expand-lg">
         <div className="container">
           <NavLink className="navbar-brand" to="/"></NavLink>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" onClick={() => setIsOpen(!isOpen)} aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
                 <NavLink className="nav-link" to="/">Home</NavLink>
