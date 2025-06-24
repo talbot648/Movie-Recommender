bdda1a23f1f441b83ad37ca34f44191897066091
talbot648 <charlietalbot648gmail.com>
2025-06-24 10:58:13 +0100

    added a framer motion transition to pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,39 @@
 import './css/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion} from "framer-motion" 
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Movies from "./pages/Movies.jsx"
 import Navbar from "./components/Navbar.jsx"
+
+function AnimatedRoutes (){
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode = "wait">
+      <Routes location={location} key = {location.pathname}>
+        <Route path="/" element= { <PageWrapper><Home /></PageWrapper>} />
+        <Route path="/About" element= { <PageWrapper><About /></PageWrapper>} />
+        <Route path="/Movies" element= { <PageWrapper><Movies /></PageWrapper>} />
+      </Routes>
+    </AnimatePresence>
+  )
+}
+
+
+function PageWrapper({children}) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y:20 }}
+      animate={{ opacity: 1, y: 0}}
+      exit={{ opacity: 0, y: 20 }}
+      transition={{ duration: 0.8 }}
+    > 
+      {children}
+    </motion.div>
+  )
+}
 
 function App() {
   
@@ -12,17 +41,13 @@ function App() {
   return (
     <>
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Navbar />} >
-    <Route index element={<Home />} />
-    <Route path="Movies" element={<Movies />} />
-    <Route path="about" element={<About />} />
-    </Route>
-    </Routes>
+    <Navbar />
+    <AnimatedRoutes />
     </BrowserRouter>
   
     </>
   )
 }
+
 
 export default App;
